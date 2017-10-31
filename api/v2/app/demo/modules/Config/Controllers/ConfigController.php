8e44641d78e40d230a7ba609240a4d1bef171423
londomloto <roso.sasongko@gmail.com>
2017-11-01 05:00:15 +0700

    .

--- api/v2/app/demo/modules/Config/Controllers/ConfigController.php
+++ api/v2/app/demo/modules/Config/Controllers/ConfigController.php
@@ -17,7 +17,7 @@ class ConfigController extends \Micro\Controller {
         }
 
         // update version
-        $data['app_version'] = '1.0.5';
+        $data['app_version'] = '1.0.6';
 
         return array(
             'success' => TRUE,
