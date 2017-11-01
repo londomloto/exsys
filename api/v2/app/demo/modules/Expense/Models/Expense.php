8603186e8650df6a478e56cba012a2ec6a9f8527
londomloto <roso.sasongko@gmail.com>
2017-11-01 13:51:25 +0700

    .

--- api/v2/app/demo/modules/Expense/Models/Expense.php
+++ api/v2/app/demo/modules/Expense/Models/Expense.php
@@ -523,11 +523,13 @@ class Expense extends \Micro\Model {
                 }
 
                 $expense[$code]['expense_value_formatted'] = number_format($expense[$code]['expense_value'], 2, ',', '.');
-                $remains[$code]['remains_value_formatted'] = number_format($remains[$code]['remains_value'], 2, ',', '.');
 
                 if ($remains[$code]['remains_value'] < 0) {
-                    $remains[$code]['remains_value_formatted'] = '('.$remains[$code]['remains_value_formatted'].')';
+                    $remains[$code]['remains_value_formatted'] = '('.number_format(abs($remains[$code]['remains_value']), 2, ',', '.').')';
+                } else {
+                    $remains[$code]['remains_value_formatted'] = number_format($remains[$code]['remains_value'], 2, ',', '.');
                 }
+                
             }
         }
 
